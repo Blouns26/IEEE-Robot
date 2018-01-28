a48b7d9ba2bf56434f8e362aa762bf843b8e64ce
Blouns26 <35809848+Blouns26@users.noreply.github.com>
2018-01-28 15:22:06 -0600

    Updated 1/28/2018
    added Kalman filter for MPU6050

--- tabed_IEEE/tabed_IEEE.ino
+++ tabed_IEEE/tabed_IEEE.ino
@@ -1,11 +1,32 @@
-//////////////////////////////////////////////////////
-///////////////////////GLOBAL////////////////////////
-////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+//////////////////////GLOBAL////////////////////////
+///////////////////////////////////////////////////
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <VL53L0X.h>
 #include <math.h>
+#include "Kalman.h" // Source: https://github.com/TKJElectronics/KalmanFilter
+
+#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+/* IMU Data */
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+// TODO: Make calibration routine
+
 
 //////////Encoders///////////
 int val;
@@ -134,6 +155,8 @@ void loop()
     Moving_average_Left();
     Moving_average_Right();
   MPU_loop();
+}
+  /*
   Serial.println("Raw Sensor value Back sensor");
   Serial.println(distance_Rear());
   Serial.println("Raw Sensor value Right sensor");
